0b22e1872403820e30efd39547809ab4ce71c1f5
artur <khalikart@gmail.com>
2022-08-04 00:48:38 +0500

    changed variables names in scripts

--- scripts.py
+++ scripts.py
@@ -11,22 +11,22 @@ django.setup()
 from table.models import Order
 
 
-def get_usd():
-    url_cb = 'https://www.cbr.ru/scripts/XML_daily.asp'
-    response = requests.get(url_cb)
-    str_python = response.content.decode('windows-1252', 'ignore')
+def get_usd_rate():
+    URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
+    response = requests.get(URL)
+    exchange_rates = response.content.decode('windows-1252', 'ignore')
     
-    root = ET.fromstring(str_python)
+    root = ET.fromstring(exchange_rates)
     for child in root:
         if child.attrib == {'ID': 'R01235'}:
             usd_str = child[4].text
-            curs_usd = usd_str.replace(',', '.')
-            return curs_usd
+            usd_rate = usd_str.replace(',', '.')
+            return usd_rate
 
-def get_sheets():       
+def get_orders_table():       
     CREDENTIALS_FILE = 'creds.json'
+    SPREADSHEET_ID = '1C4gRZeZ1C-fqDkUcjRzqbW5qvx_rZYlwj_FNrte3pGI'
     
-    spreadsheet_id = '1C4gRZeZ1C-fqDkUcjRzqbW5qvx_rZYlwj_FNrte3pGI'
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         CREDENTIALS_FILE,
         ['https://www.googleapis.com/auth/spreadsheets',
@@ -36,23 +36,23 @@ def get_sheets():
     service = build('sheets', 'v4', http=httpAuth)
 
     values = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id,
+        spreadsheetId=SPREADSHEET_ID,
         range='A2:500',
         majorDimension='ROWS'
     ).execute()
     return values['values']
 
 def save_data():
-    usd_cost = get_usd()
-    orders_table = get_sheets()
+    usd_rate = get_usd_rate()()
+    orders_table = get_orders_table()()
     for row in orders_table:
         if row:
             try:
                 row[2] = int(row[2])
                 temp_r = row[3].split('.')
                 row[3] = '-'.join(temp_r[::-1])
-                cost_rub = row[2] * float(usd_cost)
-                row.append(round(cost_rub, 2))
+                cost_in_rubles = row[2] * float(usd_rate)
+                row.append(round(cost_in_rubles, 2))
                 order = Order(field_number=row[0], order_number=row[1],
                             cost_in_dollars=row[2], deliver_time=row[3],
                             cost_in_rubles=row[4])
