d1af12e38293ba2fd8de1f31c35a93a2f8b12a0f
khalikovartur <72015866+khalikovartur@users.noreply.github.com>
2022-08-04 12:24:55 +0500

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -13,15 +13,16 @@ from table.models import Order
 
 def get_usd_rate():
     URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
+    
     response = requests.get(URL)
     exchange_rates = response.content.decode('windows-1252', 'ignore')
     
-    root = ET.fromstring(exchange_rates)
-    for child in root:
-        if child.attrib == {'ID': 'R01235'}:
-            usd_str = child[4].text
-            usd_rate = usd_str.replace(',', '.')
+    exchange_rates_xml = ET.fromstring(exchange_rates)
+    for currency in exchange_rates_xml:
+        if currency.attrib == {'ID': 'R01235'}:
+            usd_rate = currency[4].text.replace(',', '.')
             return usd_rate
+
 
 def get_orders_table():       
     CREDENTIALS_FILE = 'creds.json'
@@ -31,26 +32,25 @@ def get_orders_table():
         CREDENTIALS_FILE,
         ['https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'])
-
     httpAuth = credentials.authorize(httplib2.Http())
     service = build('sheets', 'v4', http=httpAuth)
 
-    values = service.spreadsheets().values().get(
+    table_values = service.spreadsheets().values().get(
         spreadsheetId=SPREADSHEET_ID,
         range='A2:500',
         majorDimension='ROWS'
     ).execute()
-    return values['values']
+    return table_values['values']
 
-def save_data():
+def save_data_in_db():
     usd_rate = get_usd_rate()
     orders_table = get_orders_table()
     for row in orders_table:
         if row:
             try:
                 row[2] = int(row[2])
-                temp_r = row[3].split('.')
-                row[3] = '-'.join(temp_r[::-1])
+                temp_row = row[3].split('.')
+                row[3] = '-'.join(temp_row[::-1])
                 cost_in_rubles = row[2] * float(usd_rate)
                 row.append(round(cost_in_rubles, 2))
                 order = Order(field_number=row[0], order_number=row[1],
@@ -61,10 +61,11 @@ def save_data():
                 print(Exception)
         
 def main():
-    schedule.every(2).minutes.do(save_data)
+    schedule.every(2).minutes.do(save_data_in_db)
     while True:
         schedule.run_pending()
     
 if __name__ == "__main__":
     main()
    
+
