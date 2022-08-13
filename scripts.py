cc1f4c0dfa5808d6623268c2ed270466cef323e9
artur <khalikart@gmail.com>
2022-08-13 13:07:25 +0500

    made Constans global

--- scripts.py
+++ scripts.py
@@ -12,44 +12,41 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 from table.models import Order
 
+CENTR_BANK_RU_URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
+GOOGLE_CREDENTIALS = 'creds.json'
+SPREAD_SHEET_ID = '1C4gRZeZ1C-fqDkUcjRzqbW5qvx_rZYlwj_FNrte3pGI'
+TELEGRAM_TOKEN = ''  #  insert telegram token
+TELEGRAM_CHAT_ID = '-686901726'  # replace with the desired chat
 
 def get_usd_rate():
     """This function receives data on the dollar exchange rate
     in rubles from the official website of the
     Central Bank of the Russian Federation"""
     
-    URL = 'https://www.cbr.ru/scripts/XML_daily.asp'
-    
-    response = requests.get(URL)
+    response = requests.get(CENTR_BANK_RU_URL)
     exchange_rates = response.content.decode('windows-1252', 'ignore')
-    
     exchange_rates_xml = ET.fromstring(exchange_rates)
     for currency in exchange_rates_xml:
         if currency.attrib == {'ID': 'R01235'}:
             usd_rate = currency[4].text.replace(',', '.')
             return usd_rate
 
-
 def get_orders_table():  
     """This function gets the data from the document using
     the Google API made in Google Sheets"""
-         
-    CREDENTIALS_FILE = 'creds.json'
-    SPREADSHEET_ID = '1C4gRZeZ1C-fqDkUcjRzqbW5qvx_rZYlwj_FNrte3pGI'
     
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        CREDENTIALS_FILE,
+        GOOGLE_CREDENTIALS,
         ['https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'])
     httpAuth = credentials.authorize(httplib2.Http())
     service = build('sheets', 'v4', http=httpAuth)
     table_values = service.spreadsheets().values().get(
-        spreadsheetId=SPREADSHEET_ID,
+        spreadsheetId=SPREAD_SHEET_ID,
         range='A2:500',
         majorDimension='ROWS'
     ).execute()
     return table_values['values']
-
 
 def save_data_in_db():
     """This function saves data from google sheets in to 
@@ -75,16 +72,12 @@ def save_data_in_db():
                 print('Value Error. An incorrect value was passed.')
             except Exception:
                 print(Exception)
-                
-                
+                               
 def sending_notification():
     """This function checks the delivery date in Google Sheets and if
     the deadline is violated, it sends a notification to Telegram"""
     
-    TOKEN = ''  #  insert telegram token
-    
-    bot = telebot.TeleBot(TOKEN)
-    chat_id = '-686901726'  # replace with the desired chat
+    bot = telebot.TeleBot(TELEGRAM_TOKEN)
     now = datetime.now()
     
     orders_table = get_orders_table()
@@ -93,15 +86,15 @@ def sending_notification():
             try:                
                 delivery_date = datetime.strptime(row[3], '%d.%m.%Y')
                 if delivery_date < now:
-                    bot.send_message(chat_id, 
-                        f'Delivery time violated. Delivery date indicated {row[3]}.\
-                            Order number: {row[1]}.')
+                    bot.send_message(TELEGRAM_CHAT_ID, 
+                        f'Delivery time violated.\
+                        Delivery date indicated {row[3]}.\
+                        Order number: {row[1]}.')
             except ValueError:
                 print('Value Error.An incorrect value was passed.')        
             except Exception:
                 print(Exception)
-                
-        
+                      
 def main():
     """This function runs scripts according to the set schedule."""
     
